5aa435acf98afbb298e9f02a1c3f6879892a4d03
Jonas Laursen <jona8690@edu.eal.dk>
2019-12-06 22:08:58 +0100

    Fixes for wordleft functions

--- Wordguesser/src/Wordguesser/Modules/Config.fs
+++ Wordguesser/src/Wordguesser/Modules/Config.fs
@@ -1,9 +1,10 @@
-module Config
+namespace Wordguesser
 
-let HiddenChar = '*' // Character used for letters not yet guessed
-let CaseSensitive = false; // Should guesses and words be case sensitive. Eg. if the word is "Apple" and the user types "a" should it count as correct. 
-let AllowBlanks = false; // Weather the system should be allowed to pick items with spaces
-let AllowHelp = false; // Weather the system should allow the use of the "cheat" feature.
-let AllowMultiple = false; // If the user can type in multiple letters. E.g. the word is "apple". The client can type "ap" the result should now be "ap***"
+module Config =
+    let HiddenChar = '*' // Character used for letters not yet guessed
+    let CaseSensitive = false; // Should guesses and words be case sensitive. Eg. if the word is "Apple" and the user types "a" should it count as correct. 
+    let AllowBlanks = false; // Weather the system should be allowed to pick items with spaces
+    let AllowHelp = false; // Weather the system should allow the use of the "cheat" feature.
+    let AllowMultiple = false; // If the user can type in multiple letters. E.g. the word is "apple". The client can type "ap" the result should now be "ap***"
 
-let WordList = ["Apple", "Banana Pie"]; 
+    let WordList = ["Apple", "Banana Pie"]; 
--- Wordguesser/src/Wordguesser/Modules/WordLeft.fs
+++ Wordguesser/src/Wordguesser/Modules/WordLeft.fs
@@ -1,8 +1,19 @@
-﻿module WordLeft
+﻿namespace Wordguesser
 
-// Idea:
-// Input: Word to guess
-// Input: Array off characters guessed
-// String.Iter on chosen world
-// If array contains the character, output character, else output hidden char from config.
+module WordLeft =
 
+    // Idea:
+    // Input: Word to guess
+    // Input: Array off characters guessed
+    // String.Iter on chosen world
+    // If array contains the character, output character, else output hidden char from config.
+
+    // https://stackoverflow.com/questions/1158030/check-if-an-element-is-within-a-sequence
+    let charUsed (used: char seq) (char: char) = 
+        if Seq.exists ((=) char) used then 
+            char 
+        else
+            Config.HiddenChar
+
+    let wordleft (word: string) (guesses: char seq) =
+        word |> String.map (charUsed guesses)
