5fa61e3dbf8a24e93b5058db55225d237b397b60
Jonas Laursen <jona8690@easv365.dk>
2019-12-06 11:48:04 +0100

    Finished config items

--- Wordguesser/src/Wordguesser/Modules/Config.fs
+++ Wordguesser/src/Wordguesser/Modules/Config.fs
@@ -1,1 +1,7 @@
-let HiddenChar = '*'
+let HiddenChar = '*' // Character used for letters not yet guessed
+let CaseSensitive = false; // Should guesses and words be case sensitive. Eg. if the word is "Apple" and the user types "a" should it count as correct. 
+let AllowBlanks = false; // Weather the system should be allowed to pick items with spaces
+let AllowHelp = false; // Weather the system should allow the use of the "cheat" feature.
+let AllowMultiple = false; // If the user can type in multiple letters. E.g. the word is "apple". The client can type "ap" the result should now be "ap***"
+
+let WordList = []; 
