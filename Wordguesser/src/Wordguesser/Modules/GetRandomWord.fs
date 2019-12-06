bbfdd75570264b522cafb2baec78350a9fe62991
Jonas Laursen <jona8690@edu.eal.dk>
2019-12-06 22:23:23 +0100

    Fixed Getword not handling case sensitive config field

--- Wordguesser/src/Wordguesser/Modules/GetRandomWord.fs
+++ Wordguesser/src/Wordguesser/Modules/GetRandomWord.fs
@@ -1,7 +1,13 @@
 ﻿namespace Wordguesser
 
 module GetRandomWord =
+    open System
+
     let random = System.Random();
 
     let GetWord =
-        Config.WordList.[random.Next(0, Config.WordList.Length-1)]
+        let word = Config.WordList.[random.Next(0, Config.WordList.Length-1)]
+        if Config.CaseSensitive then
+           word.ToString()
+        else
+            word.ToString().ToUpper()
