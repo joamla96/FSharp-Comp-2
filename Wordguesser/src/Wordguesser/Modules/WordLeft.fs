9bd28481f140afd340c87b166786431895d858b0
Jonas Laursen <jona8690@edu.eal.dk>
2019-12-06 13:10:44 +0100

    added comments to wordLeft for implementation idea

--- Wordguesser/src/Wordguesser/Modules/WordLeft.fs
+++ Wordguesser/src/Wordguesser/Modules/WordLeft.fs
@@ -1,2 +1,8 @@
 ﻿module WordLeft
 
+// Idea:
+// Input: Word to guess
+// Input: Array off characters guessed
+// String.Iter on chosen world
+// If array contains the character, output character, else output hidden char from config.
+
