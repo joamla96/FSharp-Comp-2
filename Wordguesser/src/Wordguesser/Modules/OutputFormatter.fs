77265bf027f7ac04461bb0c15e581941397282d8
Jonas Laursen <jona8690@edu.eal.dk>
2019-12-07 11:33:08 +0100

    Added output formatter closing issue #5

---
+++ Wordguesser/src/Wordguesser/Modules/OutputFormatter.fs
@@ -0,0 +1,9 @@
+﻿namespace Wordguesser
+
+module OutputFormatter =
+    
+    let GenerateOutput (word: string) (guessed: char seq) = 
+        printfn "Word Length: %i" word.Length
+        printfn "Guessed Letters: %A" guessed
+        printfn  "%s" (WordLeft.wordleft word guessed)
+
